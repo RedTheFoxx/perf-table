902b1e17eb5759e76c93430fa8433d39ba7bc169
Red <morisseau.jeremy@gmail.com>
2025-08-20 01:00:11 +0200

    refactor: move performance counter documentation from query.c to console.c

--- console.c
+++ console.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+/* For perf counters, we need to get a handle with PdhOpenQuery, so do the following sequence :
+
+1. PdhOpenQuery
+2. PdhAddCounter
+3. PdhCollectQueryData
+4. PdhGetFormattedCounterValue
+5. PdhCloseQuery
+
+The PdhOpenQuery function creates a query handle.
+The PdhAddCounter function adds a counter to a query.
+The PdhCollectQueryData function collects the data for a query.
+The PdhGetFormattedCounterValue function retrieves the formatted counter value. 
+The PdhCloseQuery function closes a query handle.
+*/
+
 int main(void)
 {
     printf("Hello World\n");
